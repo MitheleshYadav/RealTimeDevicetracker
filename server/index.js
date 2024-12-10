d00550a58921aaf79b5cc95cfb8558e2b28c0794
MitheleshYadav <94854673+MitheleshYadav@users.noreply.github.com>
2024-12-10 15:34:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 app.use(cors());
 const io = new Server(server,{
     cors: {
-        origin : 'http://localhost:3000',
+        origin : 'https://real-time-devicetracker.vercel.app',
         methods : ['GET', 'Post']
     }
 });
